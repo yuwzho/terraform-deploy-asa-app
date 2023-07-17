8ad39d51d99ea6d256fc6133c911203886f5b43a
Yuwei Zhou <yuwzho@microsoft.com>
2023-07-17 21:54:54 +0800

    remove upload module

--- upload/output.tf
+++
@@ -1,4 +0,0 @@
-output "relative_path" {
-  value       = jsondecode(azapi_resource_action.get_resource_upload_url.output).relativePath
-  description = "relative path that can feed to Azure Spring Apps deployment or build."
-}
--- upload/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "resource_id" {}
-variable "source_url" {}
-variable "auth_header" {
-  default = "no-auth"
-}
-
-variable "resource_group_name" {}
-variable "location" {}
